74ce3f1c1ac63cd0e7f0f5edb9ffa283333e48b4
sbomarketplace <sbomarketplaceapp@gmail.com>
2025-08-24 14:18:07 -0700

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,28 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.clearcase.app',
   appName: 'ClearCase',
-  webDir: 'dist',
-  plugins: {
-    LocalNotifications: {
-      smallIcon: "ic_stat_icon_config_sample",
-      iconColor: "#488AFF",
-      sound: "beep.wav",
-    },
-    PrivacyScreen: {
-      enable: true,
-      showInAppSwitcher: false,
-      imageFilePath: "splash.png"
-    }
-  },
-  ios: {
-    contentInset: 'automatic'
-  },
-  android: {
-    buildOptions: {
-      keystorePath: undefined,
-      keystoreAlias: undefined,
-    }
-  }
+  webDir: 'dist'
 };
 
 export default config;
