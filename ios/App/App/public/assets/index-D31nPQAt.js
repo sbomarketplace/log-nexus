b8f04c53b03879bb9ff56634400d339b2641009b
sbomarketplace <sbomarketplaceapp@gmail.com>
2025-08-24 14:20:40 -0700

    Migrate to @capawesome/capacitor-app-review; add rate helpers; Capacitor 7 config

---
+++ ios/App/App/public/assets/index-D31nPQAt.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BxaBQWDN.js","assets/index-CQR_Q1gW.js"])))=>i.map(i=>d[i]);
+import{_ as e}from"./index-CWjiNskl.js";import{r as p}from"./index-CQR_Q1gW.js";const _=p("App",{web:()=>e(()=>import("./web-BxaBQWDN.js"),__vite__mapDeps([0,1])).then(r=>new r.AppWeb)});export{_ as A};
---
+++ ios/App/App/public/assets/web-B1rfplgW.js
@@ -0,0 +1,1 @@
+import{W as e}from"./index-CQR_Q1gW.js";class i extends e{async enable(){throw this.unimplemented("Not implemented on web.")}async disable(){throw this.unimplemented("Not implemented on web.")}}export{i as PrivacyScreenWeb};
